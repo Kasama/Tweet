3383f32fe378275ed16b77fc4f10b39618b319f9
HenriqueLosc <henrique.losc@gmail.com>
2015-04-19 20:39:11 -0300

    Sem prints

--- printTweet.c
+++ printTweet.c
@@ -2,14 +2,5 @@
 #include "tweet.h"
 
 char *printTweet(TWEET *tweet) {
-
-	printf("Tweet : %s\n", tweet->text);
-	printf("User : @%s\n", tweet->userName);
-	printf("Coordenates : %s\n", tweet->coords);
-	printf("Langueage : %s\n", tweet->language);
-	printf("Favortites #%d\n", tweet->favoriteCount);
-	printf("Retweets #%d\n", tweet->retweetCount);
-	printf("Views #%ld\n", tweet->viewsCount);
-
 	return tweet->text;
 }
