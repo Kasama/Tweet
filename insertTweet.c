7ada55b0c9e727e592a6b18472a464ed2b208e7d
fredmarques <frederico.marques@live.com>
2015-04-22 20:58:08 -0300

    Now it works!
    fix several bugs...
    remove commnets to test the function.

--- insertTweet.c
+++ insertTweet.c
@@ -1,4 +1,24 @@
- int insertTweet(				
+/*#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define ERROR 0
+#define SUCCESS 1
+#define TAM 30
+
+typedef struct {
+		char   fileName[TAM];       
+		char   text[TAM];           
+		char   userName[TAM];       
+		char   coords[TAM];         
+		int    favoriteCount;   
+		char   language[TAM];      
+		int    retweetCount;    
+		long   viewsCount; 
+}TWEET;
+*/
+
+int insertTweet(				
 		char   *fileName,       
 		char   *text,           
 		char   *userName,       
@@ -9,13 +29,13 @@
 		long   viewsCount       
 ){
 	FILE *tweetFile = NULL;
-	TWEET *tweet = NULL, aux = NULL;
-	int RRN = 0, flag = 0;//ler comentário abaixo...
+	TWEET *tweet = NULL, *aux = NULL;
+	int RRN = -1, flag = 0;//ler comentário abaixo...
 
-	if(tweet->favoriteCount < 0 || tweet->retweetCount < 0 || tweet->viewsCount < 0)
+	if( favoriteCount < 0 || retweetCount < 0 || viewsCount < 0)
 		return ERROR;	
 
-	tweetFile = fopen(filename, "a+");
+	tweetFile = fopen(fileName, "a+");
 	if(tweetFile == NULL)
 		return ERROR;
 
@@ -29,11 +49,17 @@
 	strcpy(tweet->coords, coords);
 	tweet->favoriteCount = favoriteCount;
 	strcpy(tweet->language, language);
+	tweet->retweetCount = retweetCount;
 	tweet->viewsCount = viewsCount;
 	
+	aux = (TWEET*) malloc(sizeof(TWEET));
+	if(aux == NULL)
+		return  ERROR;
+
 	//determinando onde serão escritos os dados
+	fseek(tweetFile, SEEK_SET, SEEK_SET);
 	while(fread(aux, sizeof(TWEET), 1, tweetFile) > 0 ){
-		if(aux->viewsCount == -1){
+		if(aux->favoriteCount == -1 && aux->retweetCount == -1  && aux->viewsCount == -1){
 			flag = 1;
 			break;
 		}
@@ -47,7 +73,7 @@
 	
 	if(flag){//se o flag for 1, vou transformar o RRN em bytes para poder colocar o ponteiro no lugar certo do arquivo
 		RRN *= sizeof(TWEET);
-		if(fsetpos(tweetFile, &RRN) == -1){
+		if(fseek(tweetFile, RRN, SEEK_SET) == -1){
 			free(tweet);
 			fclose(tweetFile);
 			return ERROR;
@@ -59,6 +85,31 @@
 		fclose(tweetFile);
 		return ERROR;	
 	}
+
 	fclose(tweetFile);
+	free(tweet);
+	free(aux);
 	return SUCCESS;
  }
+/*
+void  main(){
+	char *fileName = NULL, *text = NULL, *userName = NULL, *coords = NULL, *language = NULL;
+	int favoriteCount, retweetCount, viewsCount;
+
+	fileName = (char*) malloc(sizeof(char)*30);
+	text = (char*) malloc(sizeof(char)*30);
+	userName = (char*) malloc(sizeof(char)*30);
+	coords = (char*) malloc(sizeof(char)*30);
+	language = (char*) malloc(sizeof(char)*30);
+	favoriteCount = 1;
+	retweetCount = 341;
+	viewsCount = 23;
+	
+	strcpy(text, "fdksljfsdljf");
+	strcpy(fileName, "bla.txt");
+	strcpy(userName, "fdakf");
+	strcpy(coords, "reowir");
+	strcpy(language, "lkjsf");
+	
+	insertTweet(fileName, text, userName, coords, favoriteCount, language, retweetCount ,viewsCount);
+}*/
